6fa707ed3ee1a7490560b2e022fcc69d01f09eaf
Barnabas Debreceni <keo@users.noreply.github.com>
2021-04-21 11:13:09 +0200

    WIP

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -14,6 +14,25 @@ osascript -e 'tell application "System Preferences" to quit'
 # Ask for the administrator password upfront
 sudo -v
 
+# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Disable press-and-hold for keys in favor of key repeat.
 defaults write -g ApplePressAndHoldEnabled -bool false
 
